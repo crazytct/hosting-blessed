64fa92448561f2ced70872b45c779f8726462253
crazytct <156101760+crazytct@users.noreply.github.com>
2024-04-02 20:40:24 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -5,13 +5,6 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
-const server1 = http.createServer((req, res) => {
-    res.write("Online");
-    res.end();
-});
-
 server.listen(8080);
-server1.listen(3500);
 
-module.exports = server1;
 module.exports = server;
